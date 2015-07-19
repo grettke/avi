6a1c35bdea63870d50911b3f68b09a3d1c768704
Grant Rettke <gcr@wisdomandwonder.com>
2015-07-19 15:48:49 -0500

    Brough in provisioner.
    Had to break/refactor Org-Mode loading.

--- .emacs.el
+++ .emacs.el
@@ -1,23 +1,74 @@
 
+;; [[file:~/src/avi/avi.org::*Start%20Up][Start\ Up:1]]
+
+(setq load-prefer-newer t)
+
+;; Start\ Up:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Run-time][Run-time:1]]
+
+(defun gcr/emacs-version-check ()
+  "Enforce version compliance."
+  (interactive)
+  (when (not (and (= emacs-major-version 24)
+              (= emacs-minor-version 4)))
+    (error
+     "Incorrect Emacs runtime. Expected v24.4. Found v%s.%s"
+     (number-to-string emacs-major-version)
+     (number-to-string emacs-minor-version))))
+(gcr/emacs-version-check)
+
+;; Run-time:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Explain%20how%20to%20make%20sure%20that%20you%20have%20the%20right%20Org-Mode%20loaded][Explain\ how\ to\ make\ sure\ that\ you\ have\ the\ right\ Org-Mode\ loaded:1]]
+
 (add-to-list 'load-path "~/src/org-mode/lisp")
+
+;; Explain\ how\ to\ make\ sure\ that\ you\ have\ the\ right\ Org-Mode\ loaded:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Explain%20how%20to%20make%20sure%20that%20you%20have%20the%20right%20Org-Mode%20loaded][Explain\ how\ to\ make\ sure\ that\ you\ have\ the\ right\ Org-Mode\ loaded:1]]
 
 (add-to-list 'load-path "~/src/org-mode/contrib/lisp")
 
+;; Explain\ how\ to\ make\ sure\ that\ you\ have\ the\ right\ Org-Mode\ loaded:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Discussion][Discussion:1]]
+
 (setq org-src-tab-acts-natively nil)
 
+;; Discussion:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Configuration][Configuration:1]]
+
 (setq org-list-allow-alphabetical +1)
+
+;; Configuration:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
 
 (require 'org)
 (require 'ox-beamer)
 (require 'ox-md)
 (require 'htmlize)
 
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
+
 (require 'ox-confluence)
+
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
 
 (setq htmlize-output-type 'inline-css)
 (setq org-html-htmlize-output-type htmlize-output-type)
 
-(defconst gcr/org-version "8.2.10")
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
+
+(defconst gcr/org-version "8.3beta")
 
 (defun gcr/warn-org-version ()
   "Warn of org misconfiguration."
@@ -26,78 +77,166 @@
     (warn "Insufficient org-mode requirements. Expected %S. Found: %S " gcr/org-version (org-version))))
 (gcr/warn-org-version)
 
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
+
 (setq org-export-coding-system 'utf-8)
 
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
+
 (setq org-export-preserve-breaks nil)
+
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
 
 (setq org-todo-keywords
       '((sequence "TODO" "IN-PROGRESS" "BLOCKED" "REVIEW" "DONE")))
 
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
+
 (setq org-startup-with-inline-images (display-graphic-p))
 
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
+
 (setq org-export-copy-to-kill-ring nil)
+
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
 
 (setq org-completion-use-ido +1)
 (setq org-outline-path-complete-in-steps nil)
 (setq org-completion-use-iswitchb nil)
 
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
+
 (setq org-use-speed-commands +1)
+
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
 
 (setq org-confirm-shell-link-function 'y-or-n-p)
 
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
+
 (setq org-confirm-elisp-link-function 'y-or-n-p)
+
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
 
 (setq org-enforce-todo-dependencies +1)
 
-(gcr/on-gui
- (require 'org-mouse))
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
 
 (setq org-ellipsis "…")
 
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
+
 (setq org-hide-leading-stars +1)
+
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
 
 (setq org-fontify-emphasized-text +1)
 
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
+
 (setq org-pretty-entities +1)
+
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
 
 (setq org-highlight-latex-and-related '(latex script entities))
 
-(require 'org-ac)
-(org-ac/config-default)
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
 
 (setq org-footnote-define-inline +1)
 (setq org-footnote-auto-label 'random)
 (setq org-footnote-auto-adjust nil)
 (setq org-footnote-section nil)
 
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
+
 (setq org-catch-invisible-edits 'error)
+
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
 
 (setq org-loop-over-headlines-in-active-region t)
 
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
+
 (setq org-export-with-toc nil)
+
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
 
 (setq org-startup-folded "nofold")
 
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
+
 (setq org-image-actual-width t)
+
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
 
 (setq org-hide-emphasis-markers +1)
 
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
+
 (setq org-startup-align-all-tables +1)
 
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
+
 (setq org-html-checkbox-type 'unicode)
+
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Org][Org:1]]
 
 (defadvice org-latex-export-to-pdf (before before-org-latex-export-to-pdf activate)
   (gcr/save-all-file-buffers))
 
-(let ((pkg 'org-show))
-  (gcr/on-gui
-   (if (not (el-get-package-is-installed pkg))
-       (warn "You wanted %S to be installed, but it isnt. Fix this." pkg)
-     (let ((fil (concat (el-get-package-directory pkg) "org-show.el")))
-       (if (not (f-exists? fil))
-           (warn "You wanted %S to exist, but it doesn't. Fix this." fil)
-         (load fil)))))
-  (gcr/not-on-gui (warn "You wanted %S to be loaded, but it won't be… it doesn't work without a GUI for some reason." pkg)))
+;; Org:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Babel][Babel:1]]
 
 (defun gcr/org-babel-after-execute-hook ()
   "Personal settings for the `org-babel-after-execute-hook'."
@@ -106,7 +245,9 @@
 
 (add-hook 'org-babel-after-execute-hook 'gcr/org-babel-after-execute-hook)
 
-(require 'ob-sml nil 'noerror)
+;; Babel:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Babel][Babel:1]]
 
 (org-babel-do-load-languages
  'org-babel-load-languages
@@ -126,27 +267,58 @@
    (R . t)
    (scheme . t)
    (sh . t)
-   (sml . t)
    (sql . t)))
+
+;; Babel:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Babel][Babel:1]]
 
 (setq org-babel-use-quick-and-dirty-noweb-expansion nil)
 
+;; Babel:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Babel][Babel:1]]
+
 (setq org-src-fontify-natively nil)
+
+;; Babel:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Babel][Babel:1]]
 
 (setq org-src-preserve-indentation +1)
 
+;; Babel:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Babel][Babel:1]]
+
 (setq org-edit-src-content-indentation 0)
 
+;; Babel:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Babel][Babel:1]]
+
 (setq org-src-strip-leading-and-trailing-blank-lines t)
+
+;; Babel:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Babel][Babel:1]]
 
 (add-to-list
  'org-structure-template-alist
  '("el" "#+begin_src emacs-lisp\n?\n#+end_src" "<src lang=\"emacs-lisp\">\n?\n</src>"))
 
+;; Babel:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Babel][Babel:1]]
+
 (mapc (lambda (asc)
         (let ((org-sce-dc (downcase (nth 1 asc))))
           (setf (nth 1 asc) org-sce-dc)))
       org-structure-template-alist)
+
+;; Babel:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Babel][Babel:1]]
 
 (add-to-list
  'org-structure-template-alist
@@ -160,6 +332,10 @@
 (add-to-list
  'org-structure-template-alist
  '("d" "#+begin_src ditaa\n?\n#+end_src" "<src lang=\"ditaa\"></src>"))
+
+;; Babel:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Babel][Babel:1]]
 
 (defun gcr/org-babel-tangle (orig-fun &rest args)
   "Display tangling time."
@@ -192,20 +368,52 @@
                    (format-time-string "%Y-%m-%dT%T%z")
                    ">")))
 
+;; Babel:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Babel][Babel:1]]
+
 (setq org-babel-min-lines-for-block-output 0)
+
+;; Babel:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Babel][Babel:1]]
 
 (setq org-edit-src-auto-save-idle-delay 0)
 
+;; Babel:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Babel][Babel:1]]
+
 (setq org-edit-src-turn-on-auto-save nil)
+
+;; Babel:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Babel][Babel:1]]
 
 (setq org-src-window-setup 'current-window)
 
+;; Babel:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Babel][Babel:1]]
+
 (setq org-babel-results-keyword "NAME")
+
+;; Babel:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Babel][Babel:1]]
 
 (setq org-babel-no-eval-on-ctrl-c-ctrl-c +1)
 
+;; Babel:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Babel][Babel:1]]
+
 (setq org-babel-noweb-wrap-start "«")
 (setq org-babel-noweb-wrap-end "»")
+
+;; Babel:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Babel][Babel:1]]
 
 (defun gcr/org-edit-src-code-plus-name ()
   "Edit the well-described source code block.
@@ -223,6 +431,10 @@ Attribtion: URL `http://emacs.stackexchange.com/a/8168/341'"
     (org-edit-src-code nil nil buff-name)
     (setq buffer-file-name file-name)))
 
+;; Babel:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Babel][Babel:1]]
+
 (defun gcr/vc-next-action ()
   "If in org source block, exit it before `vc-next-action'."
   (interactive)
@@ -232,6 +444,10 @@ Attribtion: URL `http://emacs.stackexchange.com/a/8168/341'"
     (org-edit-src-exit))
   (gcr/save-all-file-buffers)
   (vc-next-action nil))
+
+;; Babel:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Babel][Babel:1]]
 
 (defun gcr/src-block-check ()
   (interactive)
@@ -246,40 +462,117 @@ Attribtion: URL `http://emacs.stackexchange.com/a/8168/341'"
                       (org-element-property :post-affiliated src-block)))))))
   (message "Source blocks checked in %s." (buffer-name (buffer-base-buffer))))
 
+;; Babel:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Babel][Babel:1]]
+
 (setq org-edit-src-code nil)
 
-(let* ((allowed '(exports
-                  file
-                  noweb
-                  noweb-ref
-                  session
-                  tangle))
-       (new-ls
-        (--filter (member (car it) allowed)
-                  org-babel-common-header-args-w-values)))
-  (setq org-babel-common-header-args-w-values new-ls))
+;; Babel:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Core%20Babel%20Configuration][Core\ Babel\ Configuration:1]]
+
+(defun gcr/set-org-babel-default-header-args (property value)
+  "Easily set system header arguments in org mode.
+
+PROPERTY is the system-wide value that you would like to modify.
+
+VALUE is the new value you wish to store.
+
+Attribution: URL `http://orgmode.org/manual/System_002dwide-header-arguments.html#System_002dwide-header-arguments'"
+  (setq org-babel-default-header-args
+        (cons (cons property value)
+              (assq-delete-all property org-babel-default-header-args))))
+
+(defun gcr/set-org-babel-default-inline-header-args (property value)
+  "See `gcr/set-org-babel-default-header-args'; same but for inline header args."
+  (setq org-babel-default-inline-header-args
+        (cons (cons property value)
+              (assq-delete-all property org-babel-default-inline-header-args))))
+
+(defun gcr/set-org-babel-default-header-args:R (property value)
+  "See `gcr/set-org-babel-default-header-args'; same but for R.
+
+This is a copy and paste. Additional languages would warrant a refactor."
+  (setq org-babel-default-header-args:R
+        (cons (cons property value)
+              (assq-delete-all property org-babel-default-header-args:R))))
+
+(defun gcr/ispell-org-header-lines-regexp (h)
+  "Help ispell ignore org header lines."
+  (interactive)
+  (cons (concat "^#\\+" h ":") ".$"))
+
+(defun gcr/ispell-a2isra (block-def)
+  "Add to the ispell skip region alist the BLOCK-DEF."
+  (interactive)
+  (add-to-list 'ispell-skip-region-alist block-def))
+
+;; Core\ Babel\ Configuration:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Comments%20%5Bfn:1e1a7e1f:%20http://orgmode.org/manual/comments.html#comments%5D][Comments\ \[fn:1e1a7e1f:\ http://orgmode\.org/manual/comments\.html\#comments\]:1]]
 
 (gcr/set-org-babel-default-header-args :comments "noweb")
 
+;; Comments\ \[fn:1e1a7e1f:\ http://orgmode\.org/manual/comments\.html\#comments\]:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Results%20%5Bfn:1625d11f:%20http://orgmode.org/manual/results.html#results%5D][Results\ \[fn:1625d11f:\ http://orgmode\.org/manual/results\.html\#results\]:1]]
+
 (gcr/set-org-babel-default-header-args :results "output replace")
+
+;; Results\ \[fn:1625d11f:\ http://orgmode\.org/manual/results\.html\#results\]:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Exports%20%5Bfn:7dad95aa:%20http://orgmode.org/manual/exports.html#exports%5D][Exports\ \[fn:7dad95aa:\ http://orgmode\.org/manual/exports\.html\#exports\]:1]]
 
 (gcr/set-org-babel-default-header-args :exports "both")
 
+;; Exports\ \[fn:7dad95aa:\ http://orgmode\.org/manual/exports\.html\#exports\]:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Noweb%20%5Bfn:3da67e2d:%20http://orgmode.org/manual/noweb.html#noweb%5D][Noweb\ \[fn:3da67e2d:\ http://orgmode\.org/manual/noweb\.html\#noweb\]:1]]
+
 (gcr/set-org-babel-default-header-args :noweb "no-export")
 
+;; Noweb\ \[fn:3da67e2d:\ http://orgmode\.org/manual/noweb\.html\#noweb\]:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Padline%20%5Bfn:508104fc:%20http://orgmode.org/manual/padline.html%5D][Padline\ \[fn:508104fc:\ http://orgmode\.org/manual/padline\.html\]:1]]
+
 (gcr/set-org-babel-default-header-args :padline "yes")
+
+;; Padline\ \[fn:508104fc:\ http://orgmode\.org/manual/padline\.html\]:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Putting%20the%20Pieces%20Together][Putting\ the\ Pieces\ Together:1]]
 
 (setq org-confirm-babel-evaluate nil)
 (gcr/set-org-babel-default-header-args :eval "always")
 
+;; Putting\ the\ Pieces\ Together:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Putting%20the%20Pieces%20Together][Putting\ the\ Pieces\ Together:1]]
+
 (setq org-export-babel-evaluate 'inline-only)
+
+;; Putting\ the\ Pieces\ Together:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Putting%20the%20Pieces%20Together][Putting\ the\ Pieces\ Together:1]]
 
 (gcr/set-org-babel-default-inline-header-args :eval "always")
 (gcr/set-org-babel-default-inline-header-args :results "value replace")
 
+;; Putting\ the\ Pieces\ Together:1 ends here
+
+;; [[file:~/src/avi/avi.org::*%E2%84%9D%20Specific][ℝ\ Specific:1]]
+
 (gcr/set-org-babel-default-header-args:R :session "*R*")
 
+;; ℝ\ Specific:1 ends here
+
+;; [[file:~/src/avi/avi.org::*KOMA-Script][KOMA-Script:1]]
+
 (eval-after-load 'ox '(require 'ox-koma-letter))
+
+;; KOMA-Script:1 ends here
+
+;; [[file:~/src/avi/avi.org::*KOMA-Script][KOMA-Script:1]]
 
 (eval-after-load 'ox-koma-letter
   '(progn
@@ -291,27 +584,67 @@ Attribtion: URL `http://emacs.stackexchange.com/a/8168/341'"
 
      (setq org-koma-letter-default-class "my-letter")))
 
+;; KOMA-Script:1 ends here
+
+;; [[file:~/src/avi/avi.org::*KOMA-Script][KOMA-Script:1]]
+
 (setq org-koma-letter-class-option-file "UScommercial9 KomaDefault")
+
+;; KOMA-Script:1 ends here
+
+;; [[file:~/src/avi/avi.org::*ispell][ispell:1]]
 
 (add-to-list 'ispell-skip-region-alist '("^#\\+begin_src ". "#\\+end_src$"))
 
+;; ispell:1 ends here
+
+;; [[file:~/src/avi/avi.org::*ispell][ispell:1]]
+
 (add-to-list 'ispell-skip-region-alist '("^#\\+BEGIN_SRC ". "#\\+END_SRC$"))
+
+;; ispell:1 ends here
+
+;; [[file:~/src/avi/avi.org::*ispell][ispell:1]]
 
 (add-to-list 'ispell-skip-region-alist '("^#\\+begin_example ". "#\\+end_example$"))
 (add-to-list 'ispell-skip-region-alist '("^#\\+BEGIN_EXAMPLE ". "#\\+END_EXAMPLE$"))
 
+;; ispell:1 ends here
+
+;; [[file:~/src/avi/avi.org::*ispell][ispell:1]]
+
 (add-to-list 'ispell-skip-region-alist '("\:PROPERTIES\:$" . "\:END\:$"))
+
+;; ispell:1 ends here
+
+;; [[file:~/src/avi/avi.org::*ispell][ispell:1]]
 
 (add-to-list 'ispell-skip-region-alist '("\\[fn:.+:" . "\\]"))
 
+;; ispell:1 ends here
+
+;; [[file:~/src/avi/avi.org::*ispell][ispell:1]]
+
 (add-to-list 'ispell-skip-region-alist '("^http" . "\\]"))
+
+;; ispell:1 ends here
+
+;; [[file:~/src/avi/avi.org::*ispell][ispell:1]]
 
 (add-to-list 'ispell-skip-region-alist '("=.*" . ".*="))
 
+;; ispell:1 ends here
+
+;; [[file:~/src/avi/avi.org::*ispell][ispell:1]]
+
 (add-to-list 'ispell-skip-region-alist '("- \\*.+" . ".*\\*: "))
 
-(let (void)
-  (--each
+;; ispell:1 ends here
+
+;; [[file:~/src/avi/avi.org::*ispell][ispell:1]]
+
+(mapc (lambda (it)
+        (gcr/ispell-a2isra (gcr/ispell-org-header-lines-regexp it)))
       '("ATTR_LATEX"
         "AUTHOR"
         "CREATOR"
@@ -336,10 +669,17 @@ Attribtion: URL `http://emacs.stackexchange.com/a/8168/341'"
         "OPTIONS"
         "SELECT_TAGS"
         "STARTUP"
-        "TITLE")
-    (gcr/ispell-a2isra (gcr/ispell-org-header-lines-regexp it))))
+        "TITLE"))
+
+;; ispell:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Hooks][Hooks:1]]
 
 (define-key org-mode-map (kbd "C-,") (lambda () (interactive) (insert " \\larr ")))
+
+;; Hooks:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Hooks][Hooks:1]]
 
 (defun gcr/org-mode-hook ()
   (local-set-key (kbd "C-;") 'gcr/vc-next-action)
@@ -379,8 +719,45 @@ Attribtion: URL `http://emacs.stackexchange.com/a/8168/341'"
 
 (add-hook 'org-mode-hook 'gcr/org-mode-hook)
 
+;; Hooks:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Hooks][Hooks:1]]
+
 (defun gcr/org-src-mode-hook ()
   (local-set-key (kbd "C-2") 'org-edit-src-exit)
   (visual-line-mode))
 
 (add-hook 'org-src-mode-hook 'gcr/org-src-mode-hook)
+
+;; Hooks:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Package%20&%20Use-Package][Package\ &\ Use-Package:1]]
+
+(package-initialize)
+
+;; Package\ &\ Use-Package:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Package%20&%20Use-Package][Package\ &\ Use-Package:1]]
+
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+
+;; Package\ &\ Use-Package:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Package%20&%20Use-Package][Package\ &\ Use-Package:1]]
+
+(add-to-list 'package-archives
+             '("gnu" . "http://elpa.gnu.org/packages/"))
+
+;; Package\ &\ Use-Package:1 ends here
+
+;; [[file:~/src/avi/avi.org::*Package%20&%20Use-Package][Package\ &\ Use-Package:1]]
+
+(defun gcr/warn-use-package ()
+  "Report use-package misconfiguration."
+  (interactive)
+  (unless (package-installed-p 'use-package)
+    (warn "Please install 'use-package by hand. To do so, restart EMACS with `-Q' then run `list-packages', choose it and install it, and finally restart EMACS.")))
+(gcr/warn-use-package)
+
+;; Package\ &\ Use-Package:1 ends here
